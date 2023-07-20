c67545b64bae63cb76ad794106b4ae1a6f376572
Abdulla <abdullabloushi@gmail.com>
2023-07-20 14:54:47 +0400

    initial stripe integration

--- iconnect-frontend/src/routes/order/+page.server.js
+++ iconnect-frontend/src/routes/order/+page.server.js
@@ -1,14 +1,35 @@
 import { redirect } from "@sveltejs/kit"
+// import dotenv from "dotenv";
+import { Stripe } from "stripe";
 
 export const prerender = false;
 export function load({ params }) {
 
 }
 
+const stripe = Stripe('sk_test_51NVJiRGxLFc3zCwXgKNjIQd7MVZ7v7DYXWDic8fwAhbKmU2EKFVEiuzIHofq4wWG22GWVTCQYqg0fyOM2DQQBLAw00i07d5S17')
 export const actions = {
     pay: async ({ request }) => {
-        const data = await request.formData();
-        console.log(`${data.get("name")} your payment is complete`);
-        throw redirect(303, '/placed');
+        // dotenv.config();
+        // console.log(process.env["STRIPE_PUBLISHABLE_KEY"]);
+        // let baseUrl = request.headers['x-forwarded-proto'] + '://' + request.headers.host;
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'aed',
+                        product_data: {
+                            name: 'ic-card',
+                        },
+                        unit_amount: 200,
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: 'http://localhost:5173/placed',
+            cancel_url: 'http://localhost:5173/',
+        });
+        throw redirect(303, session.url);
     }
 }
--- iconnect-frontend/src/routes/order/+page.svelte
+++ iconnect-frontend/src/routes/order/+page.svelte
@@ -1,16 +1,30 @@
 <script src="https://js.stripe.com/v3/">
   import { enhance } from "$app/forms";
+  import { loadStripe } from "@stripe/stripe-js";
+
+  let stripePromise = loadStripe(
+    "sk_test_51NVJiRGxLFc3zCwXgKNjIQd7MVZ7v7DYXWDic8fwAhbKmU2EKFVEiuzIHofq4wWG22GWVTCQYqg0fyOM2DQQBLAw00i07d5S17"
+  );
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const stripe = await stripePromise;
+    const response = await fetch("/your-server-route", { method: "POST" });
+    const { sessionId } = await response.json();
+    stripe.redirectToCheckout({ sessionId });
+  }
 </script>
 
 <div class="content">
   <aside />
   <main class="order-form">
     <h2>Place Your Order</h2>
-    <form method="post" action="?/pay" use:enhance>
+    <form action="?/pay" method="post" use:enhance>
       <label>
         Name:
         <input name="name" autocomplete="off" />
       </label>
+      <button on:submit={handleSubmit} type="submit">Pay</button>
     </form>
   </main>
   <aside />
