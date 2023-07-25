d87c9d16dbac142c34aca235795c38ef2af6277c
Abdulla <abdullabloushi@gmail.com>
2023-07-25 19:11:32 +0400

    polished webhook slightly, further tested, updated flask version in root requirments.txt

--- functions/main.py
+++ functions/main.py
@@ -2,66 +2,73 @@ from firebase_functions import https_fn
 from firebase_admin import initialize_app, firestore, credentials
 from flask import Flask, jsonify, request, Response
 import stripe
+from dotenv import load_dotenv
+import os
 
-cred = credentials.Certificate("")
+load_dotenv()
 
-fire_app = initialize_app()
+cred = credentials.Certificate(os.environ.get("FIREBASE_ADMIN_CRED"))
+
+fire_app = initialize_app(cred)
 db = firestore.client()
-stripe.api_key = "sk_test_51KxejGD27b5b7CLZonHYNPNf3a4YGSYFGSo7qGThNX9ryPZDumT1eaTbQgiplH6G0A6RsWDwDqpP8nnbsNGNnLMb00iqmNuqza"
-app = Flask(__name__)
+stripe.api_key = os.environ.get("STRIPE_PUBLISHABLE_KEY")
+app = Flask("internal")
+endpoint_sk = os.environ.get("STRIPE_WEBHOOK_KEY")
+print(endpoint_sk)
 
 
-@app.get("/test")
-def test_get():
-    data = request.get_json()
-
-    if "name" in data.keys():
-        response = {"response": f"Hello {data['name']}"}
-        response = jsonify(response)
-        return response
-    else:
-        return Response(status=200, response="Hello wonderful person")
-
-
-@app.post("/payment_done")
-def payment_webhook():
-    event_payload = request.get_json()
+def make_event(req_data: str, sign_header: str, endpoint_sk: str) -> stripe.Event:
     try:
-        # have to import stripe
-        event = stripe.Event.construct_from(event_payload, stripe.api_key)
-    except ValueError as e:
-        print("Something went wrong")
-        return Response(status=404, response="Invalid payload")
-        # Invalid payload
+        return stripe.Webhook.construct_event(req_data, sign_header, endpoint_sk)
+    except stripe.error.SignatureVerificationError as e:
+        print("⚠️  Webhook signature verification failed. " + str(e))
+        return None
 
-    if event.type == "payment_intent.succeeded":
-        # make a new order doc in firestore, will have to extract data
-        # email the user, will have to collect info here
-        amount = event.data["object"]["amount"]
-        charges = event.data["object"]["charges"]["data"][0]
-        billing_details = charges["billing_details"]
-        name = billing_details["name"]
-        email = billing_details["email"]
 
-        doc = {"name": name, "email": email, "price": amount / 100}
+def proc_payment(data: dict) -> None:
+    # make a new order doc in firestore, will have to extract data
+    # email the user, will have to collect info here
+    print(data)
+    # amount = data["object"]["amount"]
+    # charges = data["object"]["charges"]["data"][0]
+    # billing_details = charges["billing_details"]
+    # name = billing_details["name"]
+    # email = billing_details["email"]
 
-        print(
-            f"Payment intent successful for {name} of amount {amount / 100} with email {email}"
-        )
-        db.collection("orders").add(doc)
+    # doc = {"name": name, "email": email, "price": amount / 100}
 
-        pass
-    elif event.type == "payment_intent.attached":
-        pass
-    else:
-        # unhandled event
-        pass
+    # print(
+    #     f"Payment intent successful for {name} of amount {amount / 100} with email {email}"
+    # )
+    # db.collection("orders").add(doc)
 
-    return Response(status=200, response="Something happened")
+
+@app.route("/payment_done", methods=["POST"])
+def payment_webhook() -> Response:
+    req_data = request.get_data(as_text=True)
+    event = None
+
+    # try:
+    #     event = request.get_json()
+    #     # have to import stripe
+    #     # event = stripe.Event.construct_from(event_payload, stripe.api_key)
+    # except:
+    #     print("Something went wrong")
+    #     return Response(status=404, response="Invalid payload")
+    #     # Invalid payload
+    event = request.get_json()
+    if endpoint_sk:
+        sign_header = request.headers.get("stripe-signature")
+        event = make_event(req_data, sign_header, endpoint_sk)
+
+    if event and event.type == "payment_intent.succeeded":
+        proc_payment(event.data)
+        return Response(status=200, response="payment successful")
+    return Response(status=404, response="Invalid payload")
 
 
 # Exposing the flask app
 @https_fn.on_request()
-def flask_endpoints(req):
+def webhookrunner(req: https_fn.Request) -> https_fn.Response:
     with app.request_context(req.environ):
         return app.full_dispatch_request()
--- iconnect-frontend/src/routes/order/+page.server.js
+++ iconnect-frontend/src/routes/order/+page.server.js
@@ -1,18 +1,17 @@
 import { redirect } from "@sveltejs/kit"
-// import dotenv from "dotenv";
 import { Stripe } from "stripe";
 
 export const prerender = false;
 export function load({ params }) {
-
 }
 
-const stripe = Stripe('sk_test_51KxejGD27b5b7CLZonHYNPNf3a4YGSYFGSo7qGThNX9ryPZDumT1eaTbQgiplH6G0A6RsWDwDqpP8nnbsNGNnLMb00iqmNuqza')
+const stripe = Stripe('sk_test_51NVJiRGxLFc3zCwXgKNjIQd7MVZ7v7DYXWDic8fwAhbKmU2EKFVEiuzIHofq4wWG22GWVTCQYqg0fyOM2DQQBLAw00i07d5S17')
 export const actions = {
     pay: async ({ request }) => {
         const data = await request.formData();
         const email = data.get('email')
         const quantity = data.get('quantity')
+
         const session = await stripe.checkout.sessions.create({
             line_items: [
                 {
