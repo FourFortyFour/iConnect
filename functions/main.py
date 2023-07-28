2c83faa213b5cd49e0f00c8c4cf982f2320ed78f
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-07-28 22:08:03 +0400

    finalized cloud function webhook - deployed function works as expected

--- functions/main.py
+++ functions/main.py
@@ -2,14 +2,14 @@ from firebase_functions import https_fn
 from firebase_admin import initialize_app, firestore, credentials
 from flask import Flask, jsonify, request, Response
 import stripe
-from dotenv import load_dotenv
+import re
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-cred = credentials.Certificate(os.environ.get("FIREBASE_ADMIN_CRED"))
-
-fire_app = initialize_app(cred)
+# cred = credentials.Certificate(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+fire_app = initialize_app()
 db = firestore.client()
 stripe.api_key = os.environ.get("STRIPE_PUBLISHABLE_KEY")
 app = Flask("internal")
@@ -17,57 +17,60 @@ endpoint_sk = os.environ.get("STRIPE_WEBHOOK_KEY")
 print(endpoint_sk)
 
 
+def is_arabic(text):
+    arabic_pattern = re.compile(
+        r'[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF]+')
+    return bool(arabic_pattern.search(text))
+
+
 def make_event(req_data: str, sign_header: str, endpoint_sk: str) -> stripe.Event:
     try:
         return stripe.Webhook.construct_event(req_data, sign_header, endpoint_sk)
     except stripe.error.SignatureVerificationError as e:
-        print("⚠️  Webhook signature verification failed. " + str(e))
+        print("Webhook signature verification failed. " + str(e))
+        return None
+    except:
+        print("Some other error")
         return None
 
 
 def proc_payment(data: dict) -> None:
     # make a new order doc in firestore, will have to extract data
     # email the user, will have to collect info here
-    print(data)
-    # amount = data["object"]["amount"]
-    # charges = data["object"]["charges"]["data"][0]
-    # billing_details = charges["billing_details"]
-    # name = billing_details["name"]
-    # email = billing_details["email"]
+    customer_details = data["object"]["shipping"]
+    metadata = data["object"]["metadata"]
+    address = customer_details["address"]
+    state = address["state"]
+    country = address["country"]
+    line1 = address["line1"]
+    line2 = address["line2"]
+    name = customer_details["name"]
+    email = metadata["user_email"]
+    quantity = metadata["product_quantity"]
+    product = metadata["product_name"]
+    phone = customer_details["phone"]
+    doc = {"name": name, "email": email, "quantity": quantity, "state": state,
+           "country": country, "line1": line1, "line2": line2, "phone": phone, "product": product}
 
-    # doc = {"name": name, "email": email, "price": amount / 100}
-
-    # print(
-    #     f"Payment intent successful for {name} of amount {amount / 100} with email {email}"
-    # )
-    # db.collection("orders").add(doc)
+    db.collection("orders").add(doc)
 
 
 @app.route("/payment_done", methods=["POST"])
 def payment_webhook() -> Response:
     req_data = request.get_data(as_text=True)
-    event = None
-
-    # try:
-    #     event = request.get_json()
-    #     # have to import stripe
-    #     # event = stripe.Event.construct_from(event_payload, stripe.api_key)
-    # except:
-    #     print("Something went wrong")
-    #     return Response(status=404, response="Invalid payload")
-    #     # Invalid payload
-    event = request.get_json()
     if endpoint_sk:
         sign_header = request.headers.get("stripe-signature")
         event = make_event(req_data, sign_header, endpoint_sk)
     if event and event.type == "payment_intent.succeeded":
         proc_payment(event.data)
         return Response(status=200, response="payment successful")
+    elif event and event.type == "payment_intent.created":
+        return Response(status=200, response="payment intent startedd")
     return Response(status=404, response="Invalid payload")
 
 
 # Exposing the flask app
 @https_fn.on_request()
-def webhookrunner(req: https_fn.Request) -> https_fn.Response:
+def webhook_runner(req: https_fn.Request) -> https_fn.Response:
     with app.request_context(req.environ):
         return app.full_dispatch_request()
--- iconnect-frontend/src/routes/order/+page.server.js
+++ iconnect-frontend/src/routes/order/+page.server.js
@@ -1,38 +1,49 @@
-import { redirect } from "@sveltejs/kit"
+import { redirect } from "@sveltejs/kit";
 import { Stripe } from "stripe";
 
 export const prerender = false;
-export function load({ params }) {
-}
+export function load({ params }) {}
 
-const stripe = Stripe('sk_test_51NVJiRGxLFc3zCwXgKNjIQd7MVZ7v7DYXWDic8fwAhbKmU2EKFVEiuzIHofq4wWG22GWVTCQYqg0fyOM2DQQBLAw00i07d5S17')
+const stripe = Stripe(
+  "sk_test_51KxejGD27b5b7CLZonHYNPNf3a4YGSYFGSo7qGThNX9ryPZDumT1eaTbQgiplH6G0A6RsWDwDqpP8nnbsNGNnLMb00iqmNuqza"
+);
 export const actions = {
-    pay: async ({ request }) => {
-        const data = await request.formData();
-        const email = data.get('email')
-        const quantity = data.get('quantity')
+  pay: async ({ request }) => {
+    const data = await request.formData();
+    const email = data.get("email");
+    const quantity = data.get("quantity");
 
-        const session = await stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'aed',
-                        product_data: {
-                            name: 'ic-card',
-                        },
-                        unit_amount: 20000,
-                    },
-                    quantity: quantity,
-                },
-            ],
-            mode: 'payment',
-            customer_email: email,
-            success_url: 'http://localhost:5173/placed',
-            cancel_url: 'http://localhost:5173/',
-            shipping_address_collection: {
-                allowed_countries: ['AE']
-            }
-        });
-        throw redirect(303, session.url);
-    }
-}
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "aed",
+            product_data: {
+              name: "ic-card",
+            },
+            unit_amount: 200,
+          },
+          quantity: quantity,
+        },
+      ],
+      phone_number_collection: {
+        enabled: true,
+      },
+      mode: "payment",
+      customer_email: email,
+      payment_intent_data: {
+        metadata: {
+          user_email: email,
+          product_quantity: quantity,
+          product_name : "ic-card",
+        },
+      },
+      success_url: "http://localhost:5173/placed",
+      cancel_url: "http://localhost:5173/",
+      shipping_address_collection: {
+        allowed_countries: ["AE"],
+      },
+    });
+    throw redirect(303, session.url);
+  },
+};
