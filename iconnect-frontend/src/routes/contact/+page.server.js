b08ad2e7ae7a8c06908180791360088ae6cf71b3
Abdulla <abdullabloushi@gmail.com>
2023-08-03 16:57:27 +0400

    changes to contact page...

--- iconnect-frontend/src/routes/contact/+page.server.js
+++ iconnect-frontend/src/routes/contact/+page.server.js
@@ -1,15 +1,15 @@
 import { db } from '../../lib/firebase.js';
 import { collection, addDoc } from "firebase/firestore";
 
-
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
         const name = data.get('name');
         const email = data.get('email');
         const msg = data.get('msg');
+        const phonenum = data.get('phonenum');
 
-        const contact_msg = { name, email, msg }
+        const contact_msg = { name, email, msg, phonenum }
         const message_collection = collection(db, 'messages');
         // await addDoc(message_collection, contact_msg);
 
