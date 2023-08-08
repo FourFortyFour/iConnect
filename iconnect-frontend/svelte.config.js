b5a319b3a0bd1d8fff3152c649e7a2f280bc87e9
Abdulla <abdullabloushi@gmail.com>
2023-08-09 01:52:30 +0400

    fixed the order route and it now works with firebase serve, chagned svelte config because google says there is no need for a svelte adapter when using their webframework firebase support

--- iconnect-frontend/svelte.config.js
+++ iconnect-frontend/svelte.config.js
@@ -1,21 +1,24 @@
 // import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 // import { preprocess } from 'svelte/compiler';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: '../public',
-			assets: '../public',
-			fallback: null,
-			precompress: false,
-		}),
-		// adapter: adapter(),
-	}
+	preprocess: vitePreprocess(),
+	/* 	kit: {
+			// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+			// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+			// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+			adapter: adapter({
+				pages: '../public',
+				assets: '../public',
+				fallback: null,
+				precompress: false,
+			}),
+			// adapter: adapter(),
+		} */
+
 };
 
 export default config;
